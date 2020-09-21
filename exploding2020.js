4bfe5b2765eab59fcbcb216c5a394ef6bb1d30a5
Chance Gibbs <kcg005@shsu.edu>
2020-09-20 19:33:54 -0500

    Update exploding2020.js

--- exploding2020.js
+++ exploding2020.js
@@ -2,28 +2,28 @@ getRandomInt = (max) => (Math.floor(Math.random() * Math.floor(max))) + 1;
 rollDice = (die=10, numDice=1) => numDice > 0 ? getRandomInt(die) + rollDice(die, numDice - 1) : 0;
 
 rollExpDice = (numDice=1, isPipAvailable=0, passThresh=7, expThresh=9, pipBonus=1) => {
-  if (numDice<1) return;
-  let ret = 0;
-  while (numDice>0) {
-        rollVal = rollDice();
-        if (rollVal >= passThresh) {
-            ret +=1;
-        } else if (isPipAvailable && rollVal+pipBonus >= passThresh) {
-            ret +=1;
-            isPipAvailable=0;   
-        }
-        if (rollVal < expThresh) numDice--;
-    }   
-    return ret;
+	if (numDice<1) return;
+	let ret = 0;
+	while (numDice>0) {
+		rollVal = rollDice();
+			if (rollVal >= passThresh) {
+				ret +=1;
+			} else if (isPipAvailable && rollVal+pipBonus >= passThresh) {
+				ret +=1;
+				isPipAvailable=0;   
+			}
+			if (rollVal < expThresh) numDice--;
+		}   
+	return ret;
 }
 
 rollExpDiceDefault = (isPipAvailable=0) => rollExpDice(1, isPipAvailable, 7, 9, 1)
 
 Array.prototype.count = function() {
-    return this.reduce(function(obj, name) {
-        obj[name] = obj[name] ? ++obj[name] : 1;
-        return obj;
-    }, {});
+	return this.reduce(function(obj, name) {
+		obj[name] = obj[name] ? ++obj[name] : 1;
+		return obj;
+	}, {});
 }
 
 runCounts = (diceFunc=rollExpDiceDefault, num=100) => [...Array(num)].map((a) => diceFunc()).count();
